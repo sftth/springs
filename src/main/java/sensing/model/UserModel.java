975bc0e5425c36980418364cc48c05b93c84443b
sftth <sftth318@gmail.com>
2019-02-02 15:05:01 +0900

    Branch Commit Test

--- src/main/java/sensing/model/UserModel.java
+++ src/main/java/sensing/model/UserModel.java
@@ -9,32 +9,36 @@ public class UserModel {
 	
 	@NumberFormat(pattern="###,##0")
 	private int point;
-	
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public int getPoint() {
 		return point;
 	}
+
 	public void setPoint(int point) {
 		this.point = point;
 	}
-	
-	
 }
